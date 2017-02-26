646cf1c6163c9714259718a11fa1abf0d9927917
Divij <divij.mehra.16@gmail.com>
2017-02-26 14:20:54 +1100

    calculatory ruby file added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Calculator
+
+## Project Name
+
+ATM Terminal App
+
+## Description
+
+Create a simple calculator that can add, subtract or multiple 2 numbers.
+
+## Installation Instructions
+
+The Calculator application is written in Ruby and must have it installed (Ruby 2.4 or higher).
+
+1. Clone the repo.
+
+2. To run the file in terminal: ```$ ruby calcultor.rb```
+
+## File History
+
+### 1st attempt: calcultor.rb
+
+Features:
+- ask the user for 2 numbers
+- ask the user whether they want to add, subtract or multiply
+- return the result
+- If 42, tell the user the meaning of life
+
+## Authors
+Divij Mehra
+
+Mentorship and insight was sought from Trent, Jamie, Peter and other peer students.
+
+## Contact Info
+Reach out to me via GitHub and email (divij.mehra.16@gmail.com)
---
+++ calculator.rb
@@ -0,0 +1,53 @@
+# Create a calcultor
+# 1. ask the user for 2 numbers
+# 2. ask the user whether they want to add, subtract or multiply
+# 3. return the result
+
+# Bonus: If 42, tell the user the meaning of life
+
+def add(x, y)
+  answer = x + y
+  display (answer)
+end
+
+def minus(x, y)
+  answer = x - y
+  display(answer)
+end
+
+def multiply(x, y)
+  answer = x * y
+  display(answer)
+end
+
+def display(answer)
+  puts "Your result: #{answer}"
+  if answer == 42
+    puts "meaning of life"
+  end
+  puts "Let's go again!"
+end
+
+loop do
+  puts "Hey there - I am a calcultor! Give me two numbers to begin with please..."
+  x, y = gets.chomp.split.map(&:to_i)
+
+  puts "Do you want to add, subtract or mutliple these two numbers?"
+  answer = gets.chomp.downcase
+
+  # add
+  if answer == "add"
+    add(x,y)
+
+  # subtract
+  elsif answer == "subtract"
+    minus(x,y)
+
+  # multiply
+  elsif answer == "multiply"
+    multiply(x,y)
+  else
+    break
+    puts "Hey, please try again! I didn't recognise what you just wrote! Use numbers only."
+  end
+end
